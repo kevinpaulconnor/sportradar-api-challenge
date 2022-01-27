4d64fb352bc991cf2124311f259cd199d9179fef
Kevin Connor <kevinpaulconnor@gmail.com>
2022-01-26 21:08:28 -0600

    service.ts -> services.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express'
-import { fetchNHLEndpoint } from './service'
+import fetchNHLEndpoint from './services'
 const app = express()
 
-app.get('/', (req, res) => {
-  fetchNHLEndpoint();
+app.get('/', async (req, res) => {
+  const data = await fetchNHLEndpoint('teams');
+  console.log(data);
+  res.json ({
+    success: 'get call succeeded',
+    data: data,
+
+  })
 })
 
 app.listen(8000, () => {
--- test/unit/services.test.ts
+++ test/unit/services.test.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
-import fetchNHLEndpoint from '../../src/service';
+import fetchNHLEndpoint from '../../src/services';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-it('make api call to get data', () => {
-    // call this first
-    mockedAxios.get.mockResolvedValue('somevalue');
- 
-    fetchNHLEndpoint('blah');
-    expect(mockedAxios.get).toHaveBeenCalledWith('https://statsapi.web.nhl.com/api/v1/blah');
- })
+describe('fetchNHLEndpoint', () => {
+    it('fetches from correct baseUrl', async () => {
+        fetchNHLEndpoint('blah');
+        expect(mockedAxios.get).toHaveBeenCalledWith('https://statsapi.web.nhl.com/api/v1/blah');
+    });
+    it('returns data.data to caller', async () => {
+        mockedAxios.get.mockResolvedValue({
+            copyright: 'yo all this belongs to the nhl',
+            data: 'some data',
+        })
+        const data = await fetchNHLEndpoint('blah');
+        expect(data).toBe('some data');
+    });
+});
